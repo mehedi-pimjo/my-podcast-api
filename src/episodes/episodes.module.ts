a6e514c208c9ddded27045b21ed09b5cb463bceb
Mehedi Pimjo <mehedi.pimjo@gmail.com>
2026-01-01 12:42:04 +0600

    add config module to topics module imports

--- src/episodes/episodes.module.ts
+++ src/episodes/episodes.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from 'src/config/config.module';
 import { EpisodesController } from './episodes.controller';
 import { EpisodesService } from './episodes.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [],
   controllers: [EpisodesController],
   providers: [EpisodesService],
 })
