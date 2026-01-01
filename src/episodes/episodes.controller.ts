8cb9ae41d2482f7f25be14dc7a3aecb1aa668cd4
Mehedi Pimjo <mehedi.pimjo@gmail.com>
2026-01-01 12:23:15 +0600

    create episodes controller methods

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('episodes')
-export class EpisodesController {}
+export class EpisodesController {
+  @Get()
+  findAll() {
+    return "All episodes";
+  }
+
+  @Get("featured")
+  findFeatured() {
+    return "Featured episodes";
+  }
+
+  @Post()
+  create() {
+    return "New episode";
+  }
+}
